264d570f1f19bdce48c225c5090a0aa801164367
Michelle-l33 <michelle.lee@spartans.ut.edu>
2024-10-21 23:56:27 -0400

    [FEAT] - added html code

---
+++ public/chat-client.js
@@ -0,0 +1,6 @@
+const socket = io();
+
+document.getElementById('send-button').addEventListener('click',()=>{
+    const message  =document.getElementById('entry').ariaValueMax;
+    //add more later
+})
