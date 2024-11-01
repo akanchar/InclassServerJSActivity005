3db98cdb635ed2c65c81d480b9fc415de15de195
Dpatel0805693 <143839560+Dpatel0805693@users.noreply.github.com>
2024-11-01 13:05:02 +0530

    FIX-fixed the leave button

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
 const { Server } = require("socket.io");
-const app = express();
-const http = require('http').createServer(app);
-const io = new Server(http);
+const http = require("http");
 const User = require("./User");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -12,46 +14,40 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "chat-client.html"));
 });
 
-let users = []; // Store all users
+let users = [];
 
 io.on("connection", (socket) => {
-    console.log('A user connected');
+    console.log("A user connected");
 
-    // When a new user sends their username
-    socket.on("username", (msg) => {
-        const newUser = new User(msg);  // Create a new user
-        users.push(newUser);  // Add to users list
-        socket.username = msg; // Store username in socket for later use
-        io.emit("updateUserList", users);  // Emit updated user list to all clients
-        io.emit("show message", "userJoined", msg);
+    socket.on("username", (username) => {
+        const newUser = new User(username);
+        users.push(newUser);
+        socket.username = username;
+        io.emit("updateUserList", users);
+        io.emit("show message", "userJoined", username);
     });
 
-    // Handle user disconnect
-    socket.on("disconnect", () => {
-        if (socket.username) {
-            console.log(`${socket.username} disconnected`);
-            io.emit("show message", "userLeft", socket.username);
-            users = users.filter(user => user.name !== socket.username);
-            io.emit("updateUserList", users);
-        }
-    });
-
-    // Handle leave button click
-    socket.on("leave", () => {
-        if (socket.username) {
-            console.log(`${socket.username} is leaving the chat`);
-            io.emit("show message", "userLeft", socket.username);
-            users = users.filter(user => user.name !== socket.username);
-            io.emit("updateUserList", users);
-            socket.disconnect(); // Disconnect the socket
-        }
-    });
-
-    // Handle chat messages
     socket.on("chat message", (who, what) => {
         io.emit("show message", "newMessage", who, what);
     });
+
+    socket.on("leave", () => {
+        handleUserDisconnect(socket);
+    });
+
+    socket.on("disconnect", () => {
+        handleUserDisconnect(socket);
+    });
+
+    function handleUserDisconnect(socket) {
+        if (socket.username) {
+            users = users.filter(user => user.name !== socket.username);
+            io.emit("show message", "userLeft", socket.username);
+            io.emit("updateUserList", users);
+            console.log(`${socket.username} disconnected`);
+        }
+    }
 });
 
 const port = 8080;
-http.listen(port, () => console.log(`Server running on port ${port}`));
+server.listen(port, () => console.log(`Server running on port ${port}`));
