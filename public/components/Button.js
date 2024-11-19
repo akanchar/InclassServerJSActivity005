849c57b0659e70d9127a9b8078f39ce99c7ecaef
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2024-11-19 09:15:30 -0500

    Revert "Troy and Liz"

--- public/components/Button.js
+++
@@ -1,9 +0,0 @@
-function createButton(id, text, onClick) {
-    const button = document.createElement('button');
-    button.id = id;
-    button.textContent = text;
-    button.addEventListener('click', onClick);
-    return button;
-}
-
-window.createButton = createButton;
